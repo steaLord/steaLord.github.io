e7713136b17e3d906bba6e6337359e284a4d124b
steaLord <dsteaLord@gmail.com>
2022-01-20 20:23:59 +0600

    script rework

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 function border(elementId) {
-    document.getElementById("th_btn_1").style.borderBottomhover = "2px solid #DD3E3E";
-    document.getElementById("th_btn_2").style.borderBottomhover = "2px solid #DD3E3E";
-    document.getElementById("th_btn_3").style.borderBottomhover = "2px solid #DD3E3E";
-    document.getElementById("th_btn_4").style.borderBottomhover = "2px solid #DD3E3E";
-    document.getElementById(elementId).style.borderBottom = "2px solid #DD3E3E";
+    document.getElementById("th_btn_1").classList.remove('th_btn_active');
+    document.getElementById("th_btn_2").classList.remove('th_btn_active');
+    document.getElementById("th_btn_3").classList.remove('th_btn_active');
+    document.getElementById("th_btn_4").classList.remove('th_btn_active');
+    document.getElementById(elementId).classList.add('th_btn_active');
 }
