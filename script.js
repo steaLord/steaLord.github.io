4608fc4bd0e18267229eb4074c7806ee2e2a68ca
steaLord <dsteaLord@gmail.com>
2022-01-20 19:50:23 +0600

    asd

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 function border(elementId) {
-    document.getElementById("th_btn_1").style.borderBottom = "2px solid #DD3E3E";
-    document.getElementById("th_btn_2").style.borderBottom = "2px solid #DD3E3E";
-    document.getElementById("th_btn_3").style.borderBottom = "2px solid #DD3E3E";
+    document.getElementById("th_btn_1").style.borderBottomhover = "2px solid #DD3E3E";
+    document.getElementById("th_btn_2").style.borderBottomhover = "2px solid #DD3E3E";
+    document.getElementById("th_btn_3").style.borderBottomhover = "2px solid #DD3E3E";
     document.getElementById("th_btn_4").style.borderBottomhover = "2px solid #DD3E3E";
     document.getElementById(elementId).style.borderBottom = "2px solid #DD3E3E";
 }
